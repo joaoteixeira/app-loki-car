31d2c3127a4adbc9377ca9846d91bab3d98eeec9
João Teixeira <joao.teixeira@ifro.edu.br>
2024-03-27 22:05:43 -0400

    arquivos inicial

---
+++ start/routes.ts
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('pages/home')
+router.on('/autor').render('pages/autor')
