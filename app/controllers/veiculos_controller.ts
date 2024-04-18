7913b314928e80c9922f5d10cdcdb8b5250edade
João Teixeira <joao.teixeira@ifro.edu.br>
2024-04-17 21:53:50 -0400

    Aula - 17/04 - Validação - part. 2

--- app/controllers/veiculos_controller.ts
+++ app/controllers/veiculos_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Veiculo from '#models/veiculo'
-import { createVeiculoValidator } from '#validators/veiculo'
+import { createVeiculoValidator, messagesVeiculoProvider } from '#validators/veiculo'
 
 export default class VeiculosController {
   /**
@@ -26,20 +26,26 @@ export default class VeiculosController {
   async store({ request, response, session }: HttpContext) {
     const dados = request.all()
 
-    const dadosValidos  = await createVeiculoValidator.validate(dados)
+    console.log(dados)
+
+    const dadosValidos = await createVeiculoValidator.validate(dados, {
+      messagesProvider: messagesVeiculoProvider
+    })
+
+    console.log(dadosValidos)
 
     const veiculo = await Veiculo.create({
       marca: dadosValidos.marca,
-      modelo: request.input('modelo'),
-      anoFabricacao: request.input('anoFabricacao'),
+      // modelo: dadosValidos.modelo,
+      anoFabricacao: dadosValidos.anoFabricacao,
       anoModelo: 2000,
       renavam: 1234567,
       cor: 'cinza',
       placa: 'NZH-9J34',
       situacao: request.input('situacao'),
     })
-    
-    if(veiculo.$isPersisted) {
+
+    if (veiculo.$isPersisted) {
       session.flash('notificacao', {
         type: 'success',
         message: `Veículo ${veiculo.modelo} cadastrado com sucesso!`,
@@ -52,20 +58,20 @@ export default class VeiculosController {
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) { }
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  async edit({ params }: HttpContext) { }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) { }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params }: HttpContext) { }
 }
--- app/validators/veiculo.ts
+++ app/validators/veiculo.ts
@@ -1,16 +1,23 @@
-import vine from '@vinejs/vine'
+import vine, { SimpleMessagesProvider } from '@vinejs/vine'
 
-/**
- * Validação Veículos - create
- */
+export const messagesVeiculoProvider = new SimpleMessagesProvider({
+  'required': 'O campo {{ field }} é obrigatório',
+  'minLength': 'O campo {{ field }} deve ter pelo menos {{ min }} caracteres',
+  'withoutDecimals': 'O campo {{ field }} deve ser inteiro',
+  'min': 'O campo {{ field }} deve ser no mínimo {{ min }}',
+
+  'placa.regex': 'A placa deve ser no formato: AAA-9A99 ou AAA-9999',
+  'situacao.enum': 'A opção selecionada é inválida, a opção deve ser: liberado ou manutencao',
+})
+
 export const createVeiculoValidator = vine.compile(
-    vine.object({
-      marca: vine.string().trim().minLength(3),
-      anoFabricacao: vine.number().withoutDecimals().min(2000),
-      situacao: vine.enum(['liberado', 'manutencao']),
-      placa: vine.string().regex(/^[A-Z]{3}-[0-9][A-Z0-9][0-9]{2}$/).trim(),
+  vine.object({
+    marca: vine.string().trim().minLength(3),
+    anoFabricacao: vine.number().withoutDecimals().min(2000),
+    situacao: vine.enum(['liberado', 'manutencao']),
+    placa: vine.string().regex(/^[A-Z]{3}-[0-9][A-Z0-9][0-9]{2}$/).trim(),
 
-      // AAA-8900
-      // AAA-8A00
-    })
-  )
+    // AAA-8900
+    // AAA-8A00
+  })
+)
